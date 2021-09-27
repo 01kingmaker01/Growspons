5a5eab86070f44ee5b9aa090f75ec44650b452b1
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-09-27 20:45:30 +0530

    proj name

--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ def login(request):
     if request.method == "POST":
         email = request.GET.get("email")
         password = request.GET.get("password")
+        
         return redirect('')
     context = {}
     return render(request, 'authentication/login.html', context)
