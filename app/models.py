413f1a32603114300f32f3caaabb9a6a9633a37c
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-09-27 21:32:02 +0530

    proj name

--- app/models.py
+++ app/models.py
@@ -1,2 +1,5 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+class Influencer(models.Model):
+    influencer_id = models.OneToOneField(User, on_delete=models.CASCADE)
