f636d28fd8851654c11850dd2e93c2de8cb65d47
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-09-27 11:46:56 +0530

    login

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+
 def home(request):
     context = {}
     return render(request, 'home.html', context)
+
+def login(request):
+
+    context = {}
+    return render(request, 'authentication/login.html', context)
