02bd072b9fd6d868142755957472a0172da610af
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-10-04 13:33:03 +0530

    add

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,10 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('influencer_details', influencer_details, name="influencer_details"),
+    path('influencer_details/', influencer_details, name="influencer_details"),
+    path('dashboard/', dashboardInf, name="dashboardInf"),
+    path('add_post/', influencerPost, name="influencerPost"),
+
 
     path('login/', loginHandle, name='login'),
     path('register/', signupHandle, name='signUp'),
